e5c254e200a2faf2eef68a9d2956e42c928e27cd
Fredi Raspall <fredi@githedgehog.com>
2025-04-25 22:45:43 +0200

    frr-agent: a daemon to remotely invoke frr-reload.
    * Daemon listens on a unix stream socket awaiting for configs
    * Configs are received as a string so that no protocol is needed.
    * The daemon writes the config to a file, reads it back and
      invokes frr-reload with some args derived from its invoaction
      cmd line.
    * The daemon first invokes frr-reload with --test.
    * If that succeeds, it calls it with --reload.
    * On success, returns "Ok".
    * On failure, it returns a string with a description of the issue.
    * If the failure is due to frr-reload, little detail is given.
    * Apart from configs, the daemon can receive keepalives, to which
      it answers with an "Ok".
    
    * There are a number of parameters to customize the behavior of
      the frr-agent and most have defaults, except for the address to
      bind to. Non-frr related commands include
       --loglevel LOGLEVEL: to set the log level.
    
    And the following two, used mostly for testing.
       --always-ok: instructs the agent to always return "Ok".
         With this, the agent does not even attempt to call
         frr-reload.
       --proc-time VALUE: instructs the agent to sleep for VALUE
         seconds when processing a request, to simulate large
         processing times.
    
    The implementation does not use tokio nor multiple threads since
    there should be at most one entity connected to the agent at a time.
    
    Closes: githedgehog/dataplane#437
    
    Signed-off-by: Fredi Raspall <fredi@githedgehog.com>

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # frr-agent
+
+* A daemon to reload FRR configurations.
+* The daemon listens on a unix stream socket expecting configs and calls frr-reload to apply them.
+* All parameters come from the cmd line. The only mandatory parameter is the address to bind the unix socket to.
+* The daemon is purposedly designed to handle a single connection at a time and to disconnect if ever the decoding
+  of a message fails, immediately transitioning to accepting a new connection.
+* The frr-agent expects data to be minimally serialized as follows.
+  Every message (sent or received) has the following structure on the wire:
+```
+      length(8 octets)|genid(8 octets)|message(length octets)|
+```
+  with:
+  
+  * length = size of the message in octets, encoded in 8 octets (host endianness)
+  * genid = generation id of the message (e.g. a config or response). In keepalives it is expected to be zero.
+  * message = the actual message as a string, which can be
+      "KEEPALIVE" in keepalives or a config BLOB in requests (incoming messages)
+      "Ok" or a blob including a failure (outgoing messages)
+
+# cmd line args
+
+The complete set of cmd line args is the following:
+```
+Daemon to reload FRR configs
+
+Usage: frr-agent [OPTIONS] --sock-path <Unix socket bind path>
+
+Options:
+      --sock-path <Unix socket bind path>
+      --loglevel <Loglevel (error, warn, info, debug, trace). Defaults to debug>
+      --outdir <Directory where received configs are stored>
+      --reloader <Full path to reloader (frr-reload.bin|py)>
+      --bindir <Directory of vtysh>
+      --rundir <Directory of where frr-reload writes temp files>
+      --confdir <Directory of frr config files>
+      --vtysock <vtysh sock (UNUSED atm)>
+      --always-ok
+      --proc-time <Artificially increase processing time by this number of seconds>
+  -h, --help                                                                         Print help
+  -V, --version
+```
+
+* confdir may not be needed if the config is retrieved from the running daemons.
+
+
---
+++ src/main.rs
@@ -0,0 +1,251 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Open Network Fabric Authors
+
+// An FRR config reloader daemon
+
+use bytes::BytesMut;
+use clap::Parser;
+use std::fs;
+use std::io::Read;
+use std::io::Write;
+use std::net::Shutdown;
+use std::os::unix::fs::PermissionsExt;
+use std::os::unix::net::{UnixListener, UnixStream};
+
+use std::thread::sleep;
+use std::time::Duration;
+
+use std::path::Path;
+use std::process::exit;
+use std::str;
+use std::str::FromStr;
+#[allow(unused)]
+use tracing::{Level, debug, error, warn};
+
+use crate::reload::frr_reload;
+
+mod reload;
+pub type GenId = i64;
+
+// initialize logging
+fn init_logging(loglevel: Level) {
+    tracing_subscriber::fmt()
+        .with_level(true)
+        .with_max_level(loglevel)
+        .compact()
+        .init();
+}
+
+fn create_unix_listener(bind_addr: &str) -> Result<UnixListener, String> {
+    // clean up entry in file system
+    let _ = std::fs::remove_file(bind_addr);
+    let bind_path = Path::new(bind_addr);
+
+    // create intermediate directories if needed
+    if let Some(parent_dir) = bind_path.parent() {
+        fs::create_dir_all(parent_dir).map_err(|e| format!("Could not create sock paths: {e}"))?;
+    }
+
+    // build listener and bind it
+    let listener = UnixListener::bind(bind_addr).map_err(|e| format!("failed to bind: {e}"))?;
+    listener
+        .set_nonblocking(false)
+        .map_err(|e| format!("Failed to set blocking: {e}"))?;
+
+    // grant permissions -- FIXME, we may want this to be more strict
+    let mut perms = fs::metadata(bind_addr)
+        .map_err(|_| "Failed to retrieve path metadata".to_string())?
+        .permissions();
+    perms.set_mode(0o777);
+    fs::set_permissions(bind_addr, perms).map_err(|_| "Failure setting permissions")?;
+
+    Ok(listener)
+}
+
+fn receive_request(sock: &mut UnixStream) -> Result<(GenId, String), String> {
+    debug!("━━━━━━ Waiting for data ━━━━━━");
+
+    let mut len_buf = [0u8; 8];
+    let mut genid_buf = [0u8; 8];
+
+    sock.read_exact(&mut len_buf)
+        .map_err(|e| format!("Could not receive msg-len: {}", e.to_string()))?;
+    sock.read_exact(&mut genid_buf)
+        .map_err(|e| format!("Could not receive genid: {}", e.to_string()))?;
+
+    let msg_size = u64::from_ne_bytes(len_buf) as usize;
+    let genid = i64::from_ne_bytes(genid_buf);
+
+    let mut rx_buff = vec![0u8; msg_size];
+    sock.read_exact(&mut rx_buff)
+        .map_err(|e| format!("Could not receive request body: {}", e.to_string()))?;
+    let request = String::from_utf8(rx_buff[0..msg_size].to_vec())
+        .map_err(|e| format!("Could not decode request body: {e:?}"))?;
+
+    debug!("Successfully received request. data-len: {msg_size} octets genid:{genid}");
+    Ok((genid, request))
+}
+
+fn send_response(sock: &mut UnixStream, genid: GenId, msg: &[u8]) -> Result<(), String> {
+    /* length of data */
+    let length = msg.len() as u64;
+
+    /* assemble wire message: |length|genid|data| */
+    let mut wire_msg = BytesMut::with_capacity(msg.len() + 16);
+    wire_msg.extend_from_slice(&length.to_ne_bytes());
+    wire_msg.extend_from_slice(&genid.to_ne_bytes());
+    wire_msg.extend_from_slice(msg);
+
+    /* send wire message */
+    sock.write_all(&wire_msg)
+        .map_err(|e| format!("Failed to send message: {e}"))?;
+    debug!("Successfully sent msg. data-len: {length} genid: {genid}");
+    Ok(())
+}
+
+// build frr-reload args from cmd line. If some params are not specified, we provide our own defaults here
+// so that we can exactly log what parameters were passed (even if frr-reload has its own defaults)
+fn build_reload_args(args: &Args) -> Vec<&str> {
+    vec![
+        "--stdout",
+        "--debug",
+        "--bindir",
+        args.binddir(),
+        "--rundir",
+        args.rundir(),
+        "--confdir",
+        args.confdir(),
+    ]
+}
+
+// cmd line args the reloader accepts. Fixme: use PathBuf instead of String?
+#[derive(Parser)]
+#[command(name = "FRR reload agent")]
+#[command(version = "1.0")]
+#[command(about = "Daemon to reload FRR configs", long_about = None)]
+pub(crate) struct Args {
+    // mandatory
+    #[arg(long, value_name = "Unix socket bind path")]
+    sock_path: String,
+
+    // optional
+    #[arg(
+        long,
+        value_name = "Loglevel (error, warn, info, debug, trace). Defaults to debug"
+    )]
+    loglevel: Option<String>,
+    #[arg(long, value_name = "Directory where received configs are stored")]
+    outdir: Option<String>,
+    #[arg(long, value_name = "Full path to reloader (frr-reload.bin|py)")]
+    reloader: Option<String>,
+    #[arg(long, value_name = "Directory of vtysh")]
+    bindir: Option<String>,
+    #[arg(long, value_name = "Directory of where frr-reload writes temp files")]
+    rundir: Option<String>,
+    #[arg(long, value_name = "Directory of frr config files")]
+    confdir: Option<String>,
+    #[arg(long, value_name = "vtysh sock (UNUSED atm)")]
+    vtysock: Option<String>,
+
+    // testing-only
+    #[arg(long)]
+    always_ok: bool,
+    #[arg(
+        long,
+        value_name = "Artificially increase processing time by this number of seconds"
+    )]
+    proc_time: Option<u64>,
+}
+impl Args {
+    pub fn binddir(&self) -> &str {
+        self.bindir.as_ref().map_or("/usr/local/bin", |v| v)
+    }
+    pub fn rundir(&self) -> &str {
+        self.rundir.as_ref().map_or("/var/run/frr", |v| v)
+    }
+    pub fn confdir(&self) -> &str {
+        self.confdir.as_ref().map_or("/etc/frr", |v| v)
+    }
+    pub fn reloader(&self) -> &str {
+        self.reloader
+            .as_ref()
+            .map_or("/hedgehog/frr-reload.py", |v| v)
+    }
+    pub fn outdir(&self) -> &str {
+        self.outdir.as_ref().map_or("/tmp/configs/hedgehog", |v| v)
+    }
+    pub fn loglevel(&self) -> Result<Level, ()> {
+        if let Some(loglevel) = &self.loglevel {
+            Level::from_str(loglevel.as_ref()).map_err(|_| ())
+        } else {
+            Ok(Level::DEBUG)
+        }
+    }
+    pub fn proc_time(&self) {
+        if let Some(time) = self.proc_time {
+            debug!("Sleeping for {time} seconds...");
+            sleep(Duration::from_secs(time));
+        }
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    let loglevel = args.loglevel().expect("Bad loglevel");
+    init_logging(loglevel);
+    debug!("Starting FRR-agent...");
+
+    /* create unix sock stream listener */
+    let sock_addr = &args.sock_path;
+    let listener = match create_unix_listener(sock_addr) {
+        Ok(listener) => listener,
+        Err(e) => {
+            error!("FATAL: Failed to open unix socket: {e:?}. Exiting....");
+            exit(1);
+        }
+    };
+
+    // build args for frr-reload from cmd line as a vector
+    let frr_reload_args = build_reload_args(&args);
+
+    debug!("frr-agent listening at '{sock_addr}' started");
+    debug!("frr-agent writes configs at '{}'", &args.outdir());
+    debug!("frr-agent reloader is '{}'", &args.reloader());
+    debug!("frr-agent loglevel is '{}'", loglevel);
+
+    loop {
+        debug!("┣━━━━ Waiting for connection ━━━━━┫");
+        if let Ok((mut stream, peer)) = listener.accept() {
+            debug!("Got connection from {peer:?}");
+            loop {
+                let Ok((genid, request)) = receive_request(&mut stream) else {
+                    error!("An error occurred. Shutting down connection...");
+                    let _ = stream.shutdown(Shutdown::Both);
+                    break; /* move to accept again */
+                };
+                args.proc_time();
+                let response = if &request == "KEEPALIVE" {
+                    debug!("Got keepalive request from {peer:?}");
+                    "Ok".to_string()
+                } else if args.always_ok {
+                    warn!("This agent is running in always-ok mode and will always report SUCCESS");
+                    "Ok".to_string()
+                } else {
+                    debug!("Got config request from {peer:?} for generation {genid}");
+                    frr_reload(
+                        args.reloader(),
+                        genid,
+                        &request,
+                        args.outdir(),
+                        &frr_reload_args,
+                    )
+                };
+                if let Err(e) = send_response(&mut stream, genid, response.as_bytes()) {
+                    error!("Error sending response: {e:?}. Shutting down connection...");
+                    let _ = stream.shutdown(Shutdown::Both);
+                    break; /* move to accept again */
+                }
+            }
+        }
+    }
+}
